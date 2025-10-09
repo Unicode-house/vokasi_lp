0623c3e9affa67d1eb4fa01f086aab91062d4d9c
sicoding1120 <daffahafizhfirdaus07@gmail.com>
2025-10-09 15:37:12 +0700

    push project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import HomePage from './pages/home'
+
+function App () {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
